5e973f6e60cffd14a580fea4c7e320d715608cb5
thipages <thipages@gmail.com>
2020-08-23 07:29:11 +0200

    todos

--- src/thipages/quick/QDb.php
+++ src/thipages/quick/QDb.php
@@ -7,6 +7,8 @@ class QDb {
     public function __construct($options=[]) {
         $this->options=$options==null?self::defaultOptions():$options;
     }
+    // todo : enssure keys are present in $options
+    // todo : add primary keys as options as a new key 'pk'
     public static function defaultOptions() {
         return [
             self::prefield=>false,
